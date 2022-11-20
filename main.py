3c584cd139ddc8882bb964a753886284e3b07dc7
Said Shafoat <109177824+Msaid1013@users.noreply.github.com>
2022-11-20 12:42:55 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def collatz_Conjecture(num):
+    while num < 1:
+        print(num)
+        if (num % 2 == 0):
+            num = num//2
+        else:
+            num = 3 * num + 1
+    print(1)
+ 
+ 
+num = int(input('Enter a number: '))
+print(f'Collatz Conjecture for {num}')
+collatz_Conjecture(num)
